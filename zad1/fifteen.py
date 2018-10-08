faa44f218f61198e21d774c498ceec07415a3d36
shreq <shrek.kresh@o2.pl>
2018-10-08 19:02:46 +0200

    stuff

--- zad1/fifteen.py
+++ zad1/fifteen.py
@@ -5,11 +5,49 @@ class Fifteen:
         self.fo.truncate(0)
         self.tiles = [list(map(int, line.split())) for line in self.fi]
         self.fi.close()
+        self.neighbours = list
 
     def __del__(self):
         self.fo.close()
 
-    def find0(self):
+    def save2file(self):
+        self.fo.write(self.tiles2str())
+
+    def tiles2str(self):
+        s = ''
         for y in range(len(self.tiles)):
             for x in range(len(self.tiles[y])):
-                print(self.tiles[x][y])
+                s += str(self.tiles[y][x]) + ' '
+            s += '\n'
+        return s
+
+    def find(self, tile):
+        for y in range(len(self.tiles)):
+            for x in range(len(self.tiles[y])):
+                if self.tiles[y][x] == tile:
+                    return x, y
+        return -1, -1
+
+    def swap(self, d, x, y):
+        if d == 'u' or d == 'U':
+            if y-1 < 0:
+                raise NameError
+            self.tiles[y][x], self.tiles[y-1][x] = self.tiles[y-1][x], self.tiles[y][x]
+            self.fo.write('U')
+        elif d == 'd' or d == 'D':
+            if y+1 > len(self.tiles):
+                raise NameError
+            self.tiles[y][x], self.tiles[y+1][x] = self.tiles[y+1][x], self.tiles[y][x]
+            self.fo.write('D')
+        elif d == 'l' or d == 'L':
+            if x-1 < 0:
+                raise NameError
+            self.tiles[y][x], self.tiles[y][x-1] = self.tiles[y][x-1], self.tiles[y][x]
+            self.fo.write('L')
+        elif d == 'r' or d == 'R':
+            if x+1 > len(self.tiles[y]):
+                raise NameError
+            self.tiles[y][x], self.tiles[y][x+1] = self.tiles[y][x+1], self.tiles[y][x]
+            self.fo.write('R')
+        else:
+            raise NameError
--- zad1/main.py
+++ zad1/main.py
@@ -1,5 +1,16 @@
 from fifteen import Fifteen
 
 f = Fifteen("start.txt", "outcome.txt")
+f.save2file()
+f.fo.write('\n')
 
-print(f.tiles)
+try:
+    x, y = f.find(0)
+    f.swap('u', x, y)
+    x, y = f.find(0)
+    f.swap('l', x, y)
+except NameError:
+    print("Error")
+
+f.fo.write('\n\n')
+f.save2file()
