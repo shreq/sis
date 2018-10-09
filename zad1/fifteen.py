faa11a7ee5937a6ed6cb9c9473940f5cd5c2616d
shreq <shrek.kresh@o2.pl>
2018-10-10 00:15:30 +0200

    astar kinda works but not really

--- zad1/fifteen.py
+++ zad1/fifteen.py
@@ -1,16 +1,27 @@
 class Fifteen:
-    def __init__(self, fin, fout):
-        self.fi = open(fin, "r", encoding="utf-8")
-        self.fo = open(fout, "a", encoding="utf-8")
-        self.tiles = [list(map(int, line.split())) for line in self.fi]
-        self.fi.close()
-        self.fo.truncate(0)
+    def __init__(self, fin, fout, is_child):
+        self.is_child = is_child
+        if not is_child:
+            self.fi = open(fin, "r", encoding="utf-8")
+            self.fo = open(fout, "a", encoding="utf-8")
+            self.tiles = [list(map(int, line.split())) for line in self.fi]
+            self.fi.close()
+            self.fo.truncate(0)
+        else:
+            self.tiles = []
         self.neighbours = []
         self.moves = []
         self.undo = ""
 
     def __del__(self):
-        self.fo.close()
+        if not self.is_child:
+            self.fo.close()
+
+    def _copy_(self, tiles, neighbours, moves, undo):
+        self.tiles = tiles
+        self.neighbours = neighbours
+        self.moves = moves
+        self.undo = undo
 
     def save2file(self):                        # write current state of tiles to file
         self.fo.write(self.tiles2str())
@@ -36,27 +47,41 @@ class Fifteen:
                 raise NameError
             self.tiles[y][x], self.tiles[y-1][x] = self.tiles[y-1][x], self.tiles[y][x]
             self.undo = 'd'
-            self.fo.write('U')
+            if not self.is_child:
+               self.fo.write('U')
         elif d == 'd' or d == 'D':
             if y+1 > len(self.tiles):
                 raise NameError
             self.tiles[y][x], self.tiles[y+1][x] = self.tiles[y+1][x], self.tiles[y][x]
             self.undo = 'u'
-            self.fo.write('D')
+            if not self.is_child:
+                self.fo.write('D')
         elif d == 'l' or d == 'L':
             if x-1 < 0:
                 raise NameError
             self.tiles[y][x], self.tiles[y][x-1] = self.tiles[y][x-1], self.tiles[y][x]
             self.undo = 'r'
-            self.fo.write('L')
+            if not self.is_child:
+                self.fo.write('L')
         elif d == 'r' or d == 'R':
             if x+1 > len(self.tiles[y]):
                 raise NameError
             self.tiles[y][x], self.tiles[y][x+1] = self.tiles[y][x+1], self.tiles[y][x]
             self.undo = 'l'
-            self.fo.write('R')
+            if not self.is_child:
+                self.fo.write('R')
         else:
             raise NameError
+
+    def move_zero(self, d):
+        x, y = self.find(0)
+        success = True
+        try:
+            self.swap(d, x, y)
+        except NameError:
+            success = False
+        self.look_around()
+        return success
 
     def look_around(self):                      # checks neighbourhood of blank tile and looks for possible moves
         self.neighbours = []
@@ -83,6 +108,36 @@ class Fifteen:
         diff = 0
         for y in range(len(self.tiles)):
             for x in range(len(self.tiles[y])):
-                if self.tiles[y][x] != y * len(self.tiles) + x + 1:
+                if y == len(self.tiles)-1 and x == len(self.tiles[y])-1:
+                    if self.tiles[y][x] != 0:
+                        diff += 1
+                elif self.tiles[y][x] != y * len(self.tiles) + x + 1:
                     diff += 1
         return diff
+
+    def astar(self):
+        loops = 0
+        while self.hamming() > 0:
+            self.look_around()
+            # self.hamming()
+            best_move = ""
+            smallest_error = 99
+            child = Fifteen(self.fi, self.fo, True)
+            child._copy_(self.tiles, self.neighbours, self.moves, self.undo)
+
+            for move in self.moves:
+                child.move_zero(move)
+                ham = child.hamming()
+                if ham < smallest_error:
+                    best_move = move
+                    smallest_error = ham
+                child.move_zero(child.undo)
+            child.__del__()
+
+            if best_move != "":
+                self.move_zero(best_move)
+
+            if loops % 10000 == 0:
+                print(loops)
+            loops += 1
+        return loops
--- zad1/main.py
+++ zad1/main.py
@@ -1,22 +1,17 @@
 from fifteen import Fifteen
 
-f = Fifteen("start.txt", "outcome.txt")
+f = Fifteen("start.txt", "outcome.txt", False)
 f.save2file()
 f.fo.write('\n')
-
-try:
-    x, y = f.find(0)
-    f.swap('u', x, y)
-    f.look_around()
-    print(f.neighbours, f.moves)
-    x, y = f.find(0)
-    f.swap('l', x, y)
-    f.look_around()
-    print(f.neighbours, f.moves)
-except NameError:
-    print("Error")
-
+'''
+f.move_zero('u')
+f.move_zero('l')
+f.move_zero('u')
+f.move_zero('l')
+f.move_zero('d')
+f.undo = ''
+'''
 f.fo.write('\n\n')
 f.save2file()
 
-print(f.hamming())
+f.astar()
