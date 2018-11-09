a46af1a95409a822d14d6b3e30f6161af64c6a1f
Szymon <szymonicho@gmail.com>
2018-11-09 10:38:40 +0100

    run.py fix

--- zad1/run.py
+++ zad1/run.py
@@ -6,16 +6,19 @@ if not os.path.exists('output'):
     os.makedirs('output')
 
 
-for algorithm in ['astr', 'dfs', 'bfs']:
+for algorithm in ['astr', 'bfs', 'dfs']:
+    print(algorithm)
     if algorithm == 'astr':
-        for heuristic in ['hamm', 'manh']:
+        for heuristic in ['manh', 'hamm']:
             for file in os.listdir(directory):
                 filename = os.fsdecode(file)
-                subprocess.call(['python', 'main.py', algorithm, heuristic, 'input/' + filename, 'output/solv_' + filename,
-                                 'output/stats_' + filename])
+                subprocess.call(['python', 'main.py', algorithm, heuristic, 'input/' + filename,
+                                 'output/' + algorithm + '_' + heuristic + '_solv_' + filename,
+                                 'output/' + algorithm + '_' + heuristic + '_stats_' + filename])
     else:
         for priority in ['RDUL', 'RDLU', 'DRUL', 'DRLU', 'LUDR', 'LURD', 'ULDR', 'ULRD']:
             for file in os.listdir(directory):
                 filename = os.fsdecode(file)
-                subprocess.call(['python', 'main.py', algorithm, priority, 'input/' + filename, 'output/solv_' + filename,
-                                 'output/stats_' + filename])
+                subprocess.call(['python', 'main.py', algorithm, priority, 'input/' + filename,
+                                 'output/' + algorithm + '_' + priority + '_solv_' + filename,
+                                 'output/' + algorithm + '_' + priority + '_stats_' + filename])
